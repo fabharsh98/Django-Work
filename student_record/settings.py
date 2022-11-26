5fa4e61679454338d6281d6991257b4748eb82aa
fabharsh98 <harsh98hp@gmail.com>
2022-11-27 01:22:01 +0530

    Untrack files in .gitignore

--- student_record/settings.py
+++ student_record/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_cleanup.apps.CleanupConfig",
 ]
 
 ROOT_URLCONF = "student_record.urls"
